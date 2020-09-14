1022fd27084b5d1c827112e8cdcd8416533b2b8d
TangMing <infly123@sina.com>
2020-09-14 18:25:36 +0800

    update

--- src/FlyGetter/Controllers/FileController.cs
+++ src/FlyGetter/Controllers/FileController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,9 +26,42 @@ namespace FlyGetter.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet]
-        [HttpPost]
-        public string ExportFile()
+        [HttpGet("{json}")]
+        [HttpPost("{json}")]
+        public string ExportFile(string json)
+        {
+            _logger.LogInformation("Export file");
+
+            string filePath = Path.Combine(Environment.CurrentDirectory, $"wwwroot\\exportfiles\\");
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+            string FileName = Path.Combine(filePath, $"{DateTime.Now:yyyyMMddHHmmssffff}.txt");
+            var sb = new StringBuilder();
+            try
+            {
+                sb.Append("Name").Append('\t').Append("Age").Append('\t').Append("Value").Append('\t');
+                sb.AppendLine();
+                sb.AppendLine("--------------------------------------------------------");
+
+                sb.Append("Tom").Append('\t').Append("12").Append('\t').Append("100").Append('\t');
+                sb.AppendLine();
+
+
+                sb.Append("Nick").Append('\t').Append("2333").Append('\t').Append("1111").Append('\t');
+                sb.AppendLine();
+
+                System.IO.File.WriteAllText(FileName, sb.ToString());
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogError("Export Error", ex);
+            }
+            return FileName;
+        }
+
+        [HttpGet("excel")]
+        [HttpPost("excel")]
+        public string ExportExcelFile()
         {
             _logger.LogInformation("Export file");
 
@@ -59,8 +94,8 @@ namespace FlyGetter.Controllers
                     worksheet.Cells[5, 1].Value = "糯米";
                     worksheet.Cells[5, 2].Value = 22;
                     worksheet.Cells[5, 3].Value = 200;
-                    
-                    ep.Save();                       
+
+                    ep.Save();
                 }
             }
             catch (InvalidOperationException ex)
--- src/FlyGetter/Startup.cs
+++ src/FlyGetter/Startup.cs
@@ -69,6 +69,7 @@ namespace FlyGetter
             }
             loggerFactory.AddLog4Net();
 
+
             app.UseStaticFiles();
 
             app.UseDirectoryBrowser();
